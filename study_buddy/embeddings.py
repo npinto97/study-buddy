8a0d529c245afa3200d2095277535a10c87d6849
npinto97 <ningo1797@gmail.com>
2025-01-16 23:46:45 +0100

    Add embeddings initialization with logging

---
+++ study_buddy/embeddings.py
@@ -0,0 +1,6 @@
+from langchain_openai import OpenAIEmbeddings
+from study_buddy.config import CONFIG, logger
+
+logger.info(f"Initializing Embeddings with model: {CONFIG.embeddings.model}")
+
+embeddings = OpenAIEmbeddings(model=CONFIG.embeddings.model)
