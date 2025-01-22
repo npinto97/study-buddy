f38805d85fb730f56a1fb43bdd7d1c2f2d61b5e6
ematanzi <e.tanzi2@studenti.uniba.it>
2025-01-22 11:11:47 +0100

    Enhance docstrings for audio metadata extraction and transcription functions

--- study_buddy/data_processing/audio_handler.py
+++ study_buddy/data_processing/audio_handler.py
@@ -6,7 +6,18 @@ from study_buddy.data_processing.utils import transcribe
 def extract_metadata_local(file_path):
     """
     Extracts metadata from a local audio file.
+    Args:
+        file_path (str): The path to the audio file.
+    Returns:
+        dict: A dictionary containing the metadata of the audio file, including:
+            - filename (str): The name of the file.
+            - filepath (str): The full path to the file.
+            - duration (float, optional): The duration of the audio file in seconds (if applicable).
+            - type (str, optional): The type of the file (if applicable).
+    Raises:
+        Exception: If there is an error in extracting metadata.
     """
+    
     file_path = Path(file_path)
     metadata = {"filename": file_path.name, "filepath": str(file_path)}
     try:
@@ -23,21 +34,16 @@ def extract_metadata_local(file_path):
     return metadata
 
 
-# TODO insert this logic in an utils.py script, add try-catch block here
-# def transcribe_audio(file_path, output_text_path):
-#     """
-#     Creates a .txt file with transcription of an audio file.
-#     """
-#     result = whisper_model.transcribe(file_path, fp16=False)
-#     with open(output_text_path, "w", encoding="utf-8") as f:
-#         f.write(result["text"])
-
-
-# TODO: audio_path must be modified
 def transcribe_audio(file_path, output_text_path):
     """
-    Creates a .txt file with transcription of a video file.
+    Transcribes audio from the given file path and saves the transcription to the specified output text path.
+    Args:
+        file_path (str): The path to the audio file to be transcribed.
+        output_text_path (str): The path where the transcribed text will be saved.
+    Raises:
+        Exception: If an error occurs during the transcription process, it will be caught and printed.
     """
+    
     try:
         transcribe(file_path, output_text_path)
 
