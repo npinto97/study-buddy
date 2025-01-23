413b899efbeb6bfe0520459c2c0fe9750fa056a2
npinto97 <ningo1797@gmail.com>
2025-01-23 02:11:44 +0100

    Refactor main application flow to integrate chatbot and graph building functionality

--- study_buddy/main.py
+++ study_buddy/main.py
@@ -1,19 +1,34 @@
-from study_buddy.pipeline import graph
 from study_buddy.config import logger
-from study_buddy.utils.graph_generator import generate_graph
+from study_buddy.agents.chatbot import chatbot, chatbot_tools
+from study_buddy.modules.graph_builder import build_graph, print_graph
 
 logger.info("Starting Study Buddy Application...")
 
+
+def main():
+    graph = build_graph(chatbot, chatbot_tools)
+    print_graph(graph)
+
+    print("Graph built successfully!")
+
+    # Run chatbot loop
+    config = {"configurable": {"thread_id": "7"}}
+
+    while True:
+        user_input = input("User: ")
+        if user_input.lower() in ["quit", "exit", "q"]:
+            print("Goodbye!")
+            break
+
+        events = graph.stream(
+            {"messages": [{"role": "user", "content": user_input}]},
+            config,
+            stream_mode="values",
+        )
+        for event in events:
+            # print("DEBUG: Messages:", event["messages"])
+            event["messages"][-1].pretty_print()
+
+
 if __name__ == "__main__":
-    question = input("Enter your question: ")
-    state = {"question": question, "context": [], "answer": ""}
-
-    logger.info(f"Received user question: {question}")
-    result = graph.invoke(state)
-
-    logger.info(f"Final answer: {result['answer']}")
-    print("Answer:", result["answer"])
-
-    generate_graph(graph=graph)
-
-    logger.info("Application finished execution.")
+    main()
