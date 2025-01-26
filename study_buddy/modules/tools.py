7bbd007579002a15ca9e8c7f07e16dc773ee03a2
npinto97 <ningo1797@gmail.com>
2025-01-26 02:55:42 +0100

    Refactor retrieve_tool to use get_vector_store for improved clarity

--- study_buddy/modules/tools.py
+++ study_buddy/modules/tools.py
@@ -1,12 +1,13 @@
 from langchain_core.tools import tool
 from langchain_community.tools.tavily_search import TavilySearchResults
 
-from study_buddy.modules.vector_store import vector_store
+from study_buddy.modules.vector_store import get_vector_store
 
 
 @tool(response_format="content_and_artifact")
 def retrieve_tool(query: str):
     """Retrieve information related to a query."""
+    vector_store = get_vector_store()
     retrieved_docs = vector_store.similarity_search(query, k=2)
     serialized = "\n\n".join(
         (f"Source: {doc.metadata}\n" f"Content: {doc.page_content}")
