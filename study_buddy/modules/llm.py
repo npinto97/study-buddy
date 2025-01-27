5b25e9ce611cbd15f86596124377569ee5523028
npinto97 <ningo1797@gmail.com>
2025-01-27 03:45:26 +0100

    Remove unused modules and dependencies to streamline the project

--- study_buddy/modules/llm.py
+++
@@ -1,6 +0,0 @@
-from langchain_openai import ChatOpenAI
-from study_buddy.config import CONFIG, logger
-
-logger.info(f"Initializing LLM with model: {CONFIG.llm.model}")
-
-llm = ChatOpenAI(model=CONFIG.llm.model)
