a48d4e6102ffbfa54a73f40ddafbad898ae58efc
npinto97 <ningo1797@gmail.com>
2025-01-31 21:12:40 +0100

    Update vector store initialization to use FAISS index directory

--- study_buddy/utils/tools.py
+++ study_buddy/utils/tools.py
@@ -2,12 +2,13 @@ from langchain_core.tools import tool
 from langchain_community.tools.tavily_search import TavilySearchResults
 
 from study_buddy.utils.vector_store import get_vector_store
+from study_buddy.config import FAISS_INDEX_DIR
 
 
 @tool(response_format="content_and_artifact")
 def retrieve_tool(query: str):
     """Retrieve information related to a query."""
-    vector_store = get_vector_store()
+    vector_store = get_vector_store(FAISS_INDEX_DIR)
     retrieved_docs = vector_store.similarity_search(query, k=2)
     serialized = "\n\n".join(
         (f"Source: {doc.metadata}\n" f"Content: {doc.page_content}")
--- tests/test_vector_store.py
+++ tests/test_vector_store.py
@@ -1,9 +1,10 @@
 from study_buddy.utils.vector_store import get_vector_store
+from study_buddy.config import FAISS_INDEX_DIR
 
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
-vector_store = get_vector_store()
+vector_store = get_vector_store(FAISS_INDEX_DIR)
 # # retrieved_docs = vector_store.similarity_search("Linked open data", k=1)
 # retrieved_docs_1 = vector_store.similarity_search("Knowledge aware RS", k=1)
 # retrieved_docs_2 = vector_store.similarity_search("Large language models", k=1)
