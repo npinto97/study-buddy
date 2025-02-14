87b7dbfa40ec0082b756ce8564f079892163e73e
npinto97 <ningo1797@gmail.com>
2025-02-14 02:02:32 +0100

    Refactor chatbot loop and update import path for vector store

--- tests/test_vector_store.py
+++ tests/test_vector_store.py
@@ -1,4 +1,4 @@
-from study_buddy.utils.vector_store import get_vector_store
+from study_buddy.vectorstore_pipeline.vector_store import get_vector_store
 from study_buddy.config import FAISS_INDEX_DIR
 
 import os
