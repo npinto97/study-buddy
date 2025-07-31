cc4096026db6a3d751db9549c690aad32285a1cd
Nicolas Pinto <ningo1797@gmail.com>
2025-07-31 12:34:56 +0200

    Refactor embeddings initialization to use HuggingFaceEmbeddings instead of OpenAIEmbeddings

--- study_buddy/utils/embeddings.py
+++ study_buddy/utils/embeddings.py
@@ -1,8 +1,8 @@
-from langchain_openai import OpenAIEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from study_buddy.config import CONFIG, logger
 
 logger.info(f"Initializing Embeddings with model: {CONFIG.embeddings.model}")
 
-embeddings = OpenAIEmbeddings(model=CONFIG.embeddings.model)
+embeddings = HuggingFaceEmbeddings(model_name=CONFIG.embeddings.model)
 
 logger.info(f"Embeddings with model {CONFIG.embeddings.model} successfully initialized.")
